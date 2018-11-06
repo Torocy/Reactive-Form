bafbd632eaefb6e5f8b650653bd2f56b4351d290
Torocy Ojweke <torocy45@gmail.com>
2018-11-07 01:01:22 +0300

    registered the control

--- src/app/name-editor/name-editor.component.ts
+++ src/app/name-editor/name-editor.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+//import formcontrol to register a single form control
+import { FormControl} from '@angular/forms';
 
 @Component({
   selector: 'app-name-editor',
@@ -6,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./name-editor.component.css']
 })
 export class NameEditorComponent implements OnInit {
+  //create a new instance of the form control
+  name = new FormControl('');
 
   constructor() { }
 
