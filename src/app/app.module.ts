8afedd4a7943d95e2e14ec457ad375dd97c4cdd9
Torocy Ojweke <torocy45@gmail.com>
2018-11-07 00:36:13 +0300

    imported the reactive form module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+//To use reactive forms,you have to import ReactiveFormsModule from @angular/forms
+import { ReactiveFormsModule} from '@angular/forms';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    //add it here
+    ReactiveFormsModule
   ],
   imports: [
     BrowserModule,
