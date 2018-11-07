06d3a8b55d96509f098269e899dc28f1f730d5cf
Torocy Ojweke <torocy45@gmail.com>
2018-11-07 09:00:15 +0300

    saving form data

---
+++ src/app/profile-editor/profile-editor.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-profile-editor',
+  templateUrl: './profile-editor.component.html',
+  styleUrls: ['./profile-editor.component.css']
+})
+export class ProfileEditorComponent  {
+//added a property profileForm and set it to anew form group instance
+  profileForm = new FormGroup({
+    //iniytialize the constructor by providing objects
+    firstName : new FormControl(''),
+    lastName : new FormControl('')
+  });
+
+  onSubmit(){
+    //Use EventEmitter with form value
+    console.warn(this.profileForm.value);
+  }
+
+}
